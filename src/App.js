c8f1e1582c8b5fb1401c918518f7a229dafa93eb
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-06-18 18:43:46 -0700

    Agrega axios a la aplicacion

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Pokemon from './components/Pokemon/Pokemon';
 
+import axios from 'axios';
+console.log(axios);
 class App extends Component {
   render() {
     return (
